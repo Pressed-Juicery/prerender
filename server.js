6ef308bf20722594ca52d7a84897ff12067bdc6a
Dave Koo <dkoo761@gmail.com>
2018-06-10 23:15:38 -0700

    Pressed Juicery customizations

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+  // chromeLocation: "/Applications/Chromium.app/Contents/MacOS/Chromium",
+  pageLoadTimeout: 60000,
+  chromeFlags: [ '--user-agent="Prerender"', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+  waitAfterLastRequest: 20000,
+  port: 3005,
+  logRequests: false
+});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
